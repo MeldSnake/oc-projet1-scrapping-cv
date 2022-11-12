b6bc937c9aa1bd5bdeef4b4edbbe773723bb45c9
MeldSnake <ikeek.33@gmail.com>
2022-11-12 14:00:49 +0100

    Correction de la phase 2.
    -Ajout de la generation du fichier CSV.
    -Convertion des liens relatif vers absolu.
    -Fin de la boucle infinie.
    
    Signed-off-by: MeldSnake <ikeek.33@gmail.com>

--- phase2.py
+++ phase2.py
@@ -1,30 +1,44 @@
-from . import data
+import csv
+import data
 from bs4 import BeautifulSoup
 from requests import Session
-from .phase1 import load_book_page
+from phase1 import load_book_page
+from urllib.parse import ParseResult, urljoin, urlparse
+
 
 def load_category_page(url: str | None, req_session: Session):
     books: list[data.BookData] = []
+    url = data.get_full_url(url, None)
     if url is None:
-        return (books, None)
+        return None
+
     response = req_session.get(url)
     if not response.ok or response.status_code != 200:
-        return (books, None)
+        return None
     doc = BeautifulSoup(response.text, 'html.parser')
+
     section = doc.select_one('body>.page>.page_inner section')
     if section is None:
-        return (books, None)
+        return None
+
     articles = section.select('article.product_pod')
     for article in articles:
         links = article.select('a[href]:not([href=""])')
         if len(links) != 0:
-            # TODO Check if it is an absolute value
-            book = load_book_page(links[0].attrs["href"].strip(), req_session)
+            book = load_book_page(
+                data.get_full_url(links[0].attrs["href"].strip(), url),
+                req_session
+            )
             if book is not None:
                 books.append(book)
-    # TODO Check if it is an absolute value
+
     next_page = section.select_one('ul.pager li.next a[href]:not([href=""])')
-    return (books, next_page.attrs["href"].strip() if next_page is not None else None)
+    if next_page is not None:
+        next_page_url = data.get_full_url(next_page.attrs['href'].strip(), url)
+    else:
+        next_page_url = None
+    return (books, next_page_url)
+
 
 def load_category(url: str | None, req_session: Session):
     all_books: list[data.BookData] = []
@@ -33,5 +47,10 @@ def load_category(url: str | None, req_session: Session):
         all_books.extend(books)
     return all_books
 
+
 if __name__ == "__main__":
-    pass
+    import sys
+    session = Session()
+    if len(sys.argv) > 1:
+        books = load_category(sys.argv[1], session)
+        data.write_csv("./phase2.csv", books)
